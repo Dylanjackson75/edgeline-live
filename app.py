184fa1f825ad2616446f0252565f0b8df7adeee8
dylanjackson75-sketch <dylanjackson75@gmail.com>
2025-10-17 23:18:34 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 st.set_page_config(page_title="EdgeLine Test", layout="wide")
 st.title("EdgeLine is live ✅")
-st.write("If you can see this screen, Python & packages are correct.")
+st.write("If you can see this, runtime and packages are correct.")
